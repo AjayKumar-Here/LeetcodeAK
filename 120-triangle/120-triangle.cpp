aceab2435256b2df5457ce71a1f28917b6cbf2c9
Ajaykumar <55996515+AjayKumar-Here@users.noreply.github.com>
2022-06-14 13:28:53 +0530

    Time: 3 ms (97.79%), Space: 9.1 MB (26.09%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int minpath(vector<vector<int>> &dp, vector<vector<int>> &triangle,int i,int j,int n)
-    {
-        if(n-1 == i) return triangle[i][j];
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-         int down=triangle[i][j] + minpath(dp,triangle,i+1,j,n);
-         int diag=triangle[i][j] + minpath(dp,triangle,i+1,j+1,n);
-        
-        
-        return dp[i][j]=min(down,diag);
-    }
+
     int minimumTotal(vector<vector<int>>& triangle) 
     {
         int n=triangle.size();
         
         if(n==1) return triangle[0][0];
         
-        vector<vector<int>> dp(n,vector<int>(n,-1));
+         vector<vector<int>> dp(n,vector<int>(n,0));
         
-        return minpath(dp,triangle,0,0,n);
+        for(int i=0;i<n;i++)
+        {
+            dp[n-1][i]=triangle[n-1][i];
+        }
+        
+        for(int i=n-2;i >=0;i--)
+        {
+            for(int j=0;j < triangle[i].size();j++)
+            {
+                dp[i][j]=triangle[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+                
+            }
+        }
+                                                
+           return dp[0][0];                          
     }
 };
