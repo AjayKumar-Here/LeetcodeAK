9e0f56bb88f4107759b74fa79689ff18d55854ac
Ajaykumar <55996515+AjayKumar-Here@users.noreply.github.com>
2022-09-06 14:46:16 +0530

    Time: 1432 ms (5.00%), Space: 24 MB (36.86%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,30 +1,40 @@
 class Solution {
 public:
     int dp1[2000][2000];
-    bool palindrome(string &s,int i,int j)
+    
+    bool palindrome(string &str,int s,int e)
     {
-        if(i >= j) return dp1[i][j] = true;
-        if(dp1[i][j] != -1) return dp1[i][j];
-        if(s[i] == s[j]) return dp1[i][j] = palindrome(s, i + 1, j - 1);
-        return dp1[i][j] = false;
+        int i=s,j=e;
+        if(dp1[s][e]!=-1) dp1[s][e];
+        
+        while(s<=e)
+        {
+            if(str[s]!=str[e])
+            {
+                return dp1[s][e]=false;
+            }
+            
+            s++;
+            e--;
+        }
+        
+        return dp1[i][j]=true;
     }
     
     int minCut(string s) 
     {
-        int n=s.size();
+        int n=s.length();
         
         vector<int> dp(n+1,0);
         
         memset(dp1, -1, sizeof(dp1));
-        
-        dp[n]=0;
         
         for(int i=n-1;i>=0;i--)
         {
             int ans=INT_MAX;
             for(int j=i;j<n;j++)
             {
-               if(palindrome(s,i,j))
+                if(palindrome(s,i,j))
                 {
                     int cost=1+dp[j+1];
                     ans=min(ans,cost);
